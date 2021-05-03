aec05b14fdbe32e129b5c0f48e0a73308d9032d9
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-02 21:29:22 -0400

    Add some coffee comments for planning

---
+++ src/main/java/com/duane/menu/coffee/Coffee.java
@@ -0,0 +1,13 @@
+package com.duane.menu.coffee;
+
+import java.util.List;
+
+public class Coffee {
+
+//    private String name;
+//    private Roaster roaster; //use roaster icons!
+//    private Country country;
+//    private List<String> roasterNotes;
+//
+
+}
---
+++ src/main/java/com/duane/menu/coffee/Roaster.java
@@ -0,0 +1,9 @@
+package com.duane.menu.coffee;
+
+public class Roaster {
+
+//    private String name;
+//    private String url;
+//    private String location;
+
+}
