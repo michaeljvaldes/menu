03e5cbcf75a3543f21b4b21b4c38e84699f118bd
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-02 21:34:08 -0400

    Add coffee module

---
+++ src/main/ui/angular/src/app/coffee/coffee.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoffeesComponent } from './component/coffees/coffees.component';
+
+
+
+@NgModule({
+  declarations: [
+    CoffeesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoffeeModule { }
---
+++ src/main/ui/angular/src/app/coffee/component/coffees/coffees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffees',
+  templateUrl: './coffees.component.html',
+  styleUrls: ['./coffees.component.scss']
+})
+export class CoffeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
