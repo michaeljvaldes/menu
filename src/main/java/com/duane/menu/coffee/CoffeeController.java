d070b2de3e7705fa9aa6baef63ab4e1e61aabd50
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-04 01:21:59 -0400

    Add shared module, fix ts strictness, and change backend url mapping

--- src/main/java/com/duane/menu/coffee/CoffeeController.java
+++ src/main/java/com/duane/menu/coffee/CoffeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/coffees")
+@RequestMapping("/api/coffee")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CoffeeController {
 
---
+++ src/main/ui/angular/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+import { MatTableModule } from "@angular/material/table";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    MatTableModule
+  ]
+})
+export class SharedModule { }
