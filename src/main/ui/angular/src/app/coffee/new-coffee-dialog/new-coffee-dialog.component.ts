a59584afb0066914bc642e6edebdd79188268927
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-05 20:32:56 -0400

    Add some front-end validation for new coffee form

--- src/main/ui/angular/src/app/coffee/new-coffee-dialog/new-coffee-dialog.component.ts
+++ src/main/ui/angular/src/app/coffee/new-coffee-dialog/new-coffee-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Coffee } from "../model/coffee";
 import { MatDialogRef } from "@angular/material/dialog";
 import { CoffeeService } from "../service/coffee.service";
-import { FormControl, Validators } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-new-coffee-dialog',
@@ -11,36 +11,55 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class NewCoffeeDialogComponent implements OnInit {
 
-  private coffee: Coffee;
-  roaster: FormControl;
-  name: FormControl;
-  country: FormControl;
+  newCoffeeForm: FormGroup = this.formBuilder.group({
+    roaster: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required])
+  })
 
   constructor(
     private dialogRef: MatDialogRef<NewCoffeeDialogComponent>,
-    private coffeeService: CoffeeService
+    private coffeeService: CoffeeService,
+    private formBuilder: FormBuilder
   ) { }
 
-  ngOnInit(): void {
-    this.roaster = new FormControl('', [Validators.required]);
-    this.name = new FormControl('', [Validators.required]);
-    this.country = new FormControl('', [Validators.required]);
+  ngOnInit(): void { }
+
+  get roaster(): AbstractControl {
+    return this.newCoffeeForm.get('roaster');
+  }
+
+  get name(): AbstractControl {
+    return this.newCoffeeForm.get('name');
+  }
+
+  get country(): AbstractControl {
+    return this.newCoffeeForm.get('country');
+  }
+
+  getErrorMessage(): string {
+    return 'This field is required';
   }
 
   save() {
-    const coffee: Coffee = {
-      id: null,
-      roaster: this.roaster.value,
-      name: this.name.value,
-      country: this.country.value
-    };
+    if (!this.newCoffeeForm.valid) {
+      this.newCoffeeForm.markAllAsTouched();
+    } else {
 
-    this.coffeeService.addCoffee(coffee)
-      .subscribe(coffee => {
-          this.dialogRef.close(coffee);
-        },
-        err => {console.log('Error adding new coffee')}
-      );
+      const coffee: Coffee = {
+        id: null,
+        roaster: this.roaster.value,
+        name: this.name.value,
+        country: this.country.value
+      };
+
+      this.coffeeService.addCoffee(coffee)
+        .subscribe(coffee => {
+            this.dialogRef.close(coffee);
+          },
+          err => {console.log('Error adding new coffee')}
+        );
+    }
   }
 
   cancel() {
